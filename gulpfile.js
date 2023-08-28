63b326c517392526075207fa9638dd42807465db
template-dev <jukov801@gmail.com>
2023-08-28 03:18:46 +0300

    Added my works block and tags

--- gulpfile.js
+++ gulpfile.js
@@ -122,7 +122,7 @@ function reloadServer (done) {
 function watchFiles () {
   gulp.watch('source/sass/**/*.scss', gulp.series(processStyles));
   gulp.watch('source/sass/*.scss', gulp.series(processStyles));
-  gulp.watch('source/js/script.js', gulp.series(processScripts));
+  gulp.watch('source/js/*.js', gulp.series(processScripts));
   gulp.watch('source/*.html', gulp.series(processMarkup, reloadServer));
   gulp.watch('source/**/*.html', gulp.series(processMarkup, reloadServer));
 }
